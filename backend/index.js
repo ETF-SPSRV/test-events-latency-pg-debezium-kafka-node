5bd45241005107aad0f0c24a8b49138546e1699b
Aldin Kapetanovic <aldinkapetanovic@gmail.com>
2025-05-07 09:17:13 +0200

    morgan

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const pool = require('./db');
 const app = express();
 const PORT = 5000;
+const morgan = require('morgan');  // Import morgan
 
+app.use(morgan('dev'));  // Add morgan middleware for logging requests
 app.use(cors());
 app.use(express.json());
 
